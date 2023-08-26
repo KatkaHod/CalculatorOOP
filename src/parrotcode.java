57a9c27c0de1d4840920b7b94c72a2dc3f1dfe1c
KaterinaHodslavska <hodslavska.k@gmail.com>
2023-08-27 01:15:07 +0200

    Add hello application

---
+++ src/parrotcode.java
@@ -0,0 +1,8 @@
+package src;
+
+public class parrotcode {
+    public static void main(String[] args) {
+        System.out.println("Hallo, I am Parrot Boo and i like to repeat the words!");
+    }
+
+}
