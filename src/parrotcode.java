d21578176e6e9e641519a86a2eb896ad5948aabb
KaterinaHodslavska <hodslavska.k@gmail.com>
2023-08-28 16:42:27 +0200

    Added a class scanner, import java.util.Scanner, input by scanner.nextLine(), string input and string output.

--- src/parrotcode.java
+++ src/parrotcode.java
@@ -1,8 +1,20 @@
 package src;
+import java.util.Scanner;
 
 public class parrotcode {
     public static void main(String[] args) {
-        System.out.println("Hallo, I am Parrot Boo and i like to repeat the words!");
+        Scanner scanner = new Scanner (System.in, "Windows-1250");
+        System.out.println("Hallo, I am Parrot Eleanor and i like to repeat the words!");
+        System.out.println("Write something and I will repeat it:  ");
+
+        String input;
+        input = scanner.nextLine();
+
+        String output;
+        output = input + ", " + input + "!";
+        System.out.println(output);
+
+
     }
 
 }
