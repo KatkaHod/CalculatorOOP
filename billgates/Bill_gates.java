78351ff296d6ed4f5948a9daa61b06c5ad6480b8
KaterinaHodslavska <hodslavska.k@gmail.com>
2023-08-29 15:19:01 +0200

    Repeating the loading of values from the console.

---
+++ billgates/Bill_gates.java
@@ -0,0 +1,19 @@
+package billgates;
+import java.util.Scanner;
+public class Bill_gates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in, "Windows-1250");
+        System.out.println("Hi, what is your name?..: ");
+
+        String first_input = scanner.nextLine();
+        System.out.println(first_input);
+
+        System.out.println("What are you like?");
+        String second_input = scanner.nextLine();
+        System.out.println(second_input);
+
+        System.out.print(first_input + " is " + " a " + second_input + " person");
+        System.out.println();
+        System.out.println("Bye, have a nice day " + first_input);
+    }
+}
