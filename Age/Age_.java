6b0701d07797e98ed55391125a5eea9d8f498ec0
KaterinaHodslavska <hodslavska.k@gmail.com>
2023-08-31 00:52:07 +0200

    Scanner next line training

---
+++ Age/Age_.java
@@ -0,0 +1,29 @@
+package Age;
+
+import java.util.Scanner;
+
+public class Age_ {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in, "Windows-1250");
+        //do not forget to add always scanner and also import java.util.Scanner!!!
+
+        System.out.println("What is your name?:..");
+        String name = scanner.nextLine(); //this method give back always a string!
+
+        System.out.println("What is your surname?:..");
+        String surname = scanner.nextLine();
+
+        System.out.println("What is your age?:..");
+        String age = scanner.nextLine();
+
+        //BECAUSE I have always a string back,
+        // I have to parse it below!
+        int agee = Integer.parseInt(age); // from the string to int
+
+
+        System.out.println("I am : " + name + surname);
+       System.out.println("Your age in a two year will be: " + (agee + 2));
+
+
+    }
+}
