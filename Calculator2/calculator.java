a4092a47193476c62b5067ea89a81bf0b7a6383e
KaterinaHodslavska <hodslavska.k@gmail.com>
2023-09-16 22:11:13 +0200

    added a while statement

--- Calculator2/calculator.java
+++ Calculator2/calculator.java
@@ -3,21 +3,20 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in,"Windows-1250");
+
         System.out.println("Hi, welcome to the improved calculator!");
+        String con = "yes";
+        while (con.equals("yes")) {
 
         System.out.println("Write a first number: ");
         Double a = Double.parseDouble(scanner.nextLine()); //input of first No.
-
         System.out.println("Write a second number: ");
         Double b = Double.parseDouble(scanner.nextLine()); // input of second number
-
         System.out.println("Choose number how you want to calculate ");
-
         System.out.println(" 1 +");
         System.out.println(" 2 - ");
         System.out.println(" 3 / ");
         System.out.println(" 4 * ");
-
         int options = Integer.parseInt(scanner.nextLine()); //now the user put an option who he wants
 
         double result = 0; //I have to declare a variable and add a value 0, cannot be empty. Than I can put the values.
@@ -35,11 +34,21 @@ public class calculator {
             case 4:
                 result = a * b;
                 break;
+                         }
+        if ((options > 0) && (options < 5)) {
+            System.out.println("Result: " + result);
+        } else {
+            System.out.println("Input is not valid, choose between 1-4");
         }
-    System.out.println("The result is: " + result);
-}
+            System.out.println("Do you want to calculate one more time? Yes / No ");
+            con = scanner.nextLine();
+        }
+        System.out.println("Thanks for using this calculator!");
+    }
 
 }
+
+
 
 
 
