0d5300f88f9d469aecac5259e3f6c5fad952af82
KatkaHod <hodslavska.k@gmail.com>
2023-11-27 22:16:53 +0100

    First object application ! :-)

--- FirstOOP/Greeting.java
+++ FirstOOP/Greeting.java
@@ -1,11 +1,9 @@
 package FirstOOP;
 
 public class Greeting {
-    public static void greet(String name) { // Public is access modifier, void method does not return any value
-        System.out.println("Hi user %s%n" name); //print the text
+    public static void Greet(String name) { // Public is access modifier, void method does not return any value
+        System.out.println("Hi user " + name);
 
     }
-
-
 
 }
--- FirstOOP/HelloObjects.java
+++ FirstOOP/HelloObjects.java
@@ -1,21 +1,23 @@
 package FirstOOP;
+import java.util.Scanner;
 
 public class HelloObjects {
     public HelloObjects() {
     }
 
-    public static void main(String[] args) { // do not forget to use main method
-
+    public static void main(String[] args) {// do not forget to use main method
         Greeting greeting = new Greeting();
+        greeting.Greet("Patrick");
+        greeting.Greet("Kristina");
+
+
         // new variable greeting, Objects are stored in variables, the class name is used as a data typ
         // The key word -new- creates a new instance of class Greeting
         // I sign the new instance to new variable - greeting )
         //When is instance created, is called constructor Greeting()
-
         //greeting.greet(); // call the method from the new variable greeting
-
-        Greeting.greet("Patrick");
-        Greeting.greet("Elleanor");
+        //Greeting.greet("Patrick");
+        //Greeting.greet("Elleanor");
 
 
 
