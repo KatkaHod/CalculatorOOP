15e0f42984730b007eb95448c2d8a57a7919f7be
KatkaHod <hodslavska.k@gmail.com>
2023-12-08 19:51:37 +0100

    First OOP progr. done

--- FirstOOP/Greeting.java
+++ FirstOOP/Greeting.java
@@ -1,10 +1,16 @@
 package FirstOOP;
-
+/** The class is created to greet users, Note: Syntax Javadoc*/
 public class Greeting {
 
-    public String text;//Always add under the new class
-    public static void Greet(String name) {
-        System.out.printf("Hi user %s%n", name);
+    public String text;
+    /**
+     * Class "Greeting" greet the users
+     * Method "Greet" returns only a string value
+     * @param name User name
+     * @return  User's greeting text
+     */
+    public String Greet(String name) {
+        return String.format("%s %s",text, name); //return - exist the method and return value
     }
 
 }
--- FirstOOP/HelloObjects.java
+++ FirstOOP/HelloObjects.java
@@ -5,12 +5,16 @@ public class HelloObjects {
     public HelloObjects() {
     }
 
-    public static void main(String[] args) {// do not forget to use main method
-        Greeting greeting = new Greeting();
-        greeting.Greet("Patrick"); //I am calling the Greet method from class Greeting
-        greeting.Greet("Lukas");
+    public static void main(String[] args) {// always write main method
+        Greeting greeting = new Greeting(); //create new instance
 
+        greeting.text = "Hi User"; //value for a string
+        System.out.println(greeting.Greet("Lukas"));
+        System.out.println(greeting.Greet("Laura"));
 
+        greeting.text = "Welcome";
+        System.out.println(greeting.Greet("Lukas"));
+        System.out.println(greeting.Greet("Laura"));
 
     }
 }
