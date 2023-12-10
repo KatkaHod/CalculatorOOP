793e667fe9250ed74ffa52080fbc49d433313d4e
KatkaHod <hodslavska.k@gmail.com>
2023-12-10 22:56:29 +0100

    Setting up the objects and methods. New features added.

--- Calculator/NCArOperations.java
+++ Calculator/NCArOperations.java
@@ -3,18 +3,46 @@ package Calculator;
 /** The NumberCruncherEncapsulation class encapsulates the calculator's state (operands)
  * and behavior (addition, subtraction, multiplication, and division).
  * NumberCruncher Arithmetic Operations
+ * Setting up the (arithmetic) Objects  and their methods
  *  */
 
 public class NCArOperations {
 
-   double firstInputUser = 0; //variable of first value
-   double secondInputUser = 0; //variable of second value
+    //Variables of the first/second input (number) from the user
+    public double a;
+    public double b;
 
-    public void ArithmeticOperations() {
+    //Arithmetic calculation option number
+    public int options;
 
-
-
-
-
+    //Setting up the Object addition
+    public double addition()
+    {
+        return a + b;
     }
+    //Setting up the Object subtraction
+    public double subtraction()
+    {
+        return a - b;
+    }
+
+    //Setting up the Object multiplication
+    public double multiplication()
+    {
+        return a * b;
+    }
+    //Setting up the Object division
+    public double division()
+    {
+        return a / b;
+    }
+
+
+
+
+
+
+
+
+
 }
--- Calculator/NumberCruncherApp.java
+++ Calculator/NumberCruncherApp.java
@@ -1,14 +1,38 @@
 package Calculator;
 
+import java.util.Scanner;
+
 /** The NumberCruncherApp class serves as the entry point
  *  and interacts with the user, creating an instance of the Calculator class
- *  and performing calculations based on user input. */
-//import java.util.Scanner; ??
-//import Scanner.NextLine; ??
+ *  and performing calculations based on user input.
+ *  */
+
 public class NumberCruncherApp {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         NCArOperations ncaroperations = new NCArOperations();
         //after set up the methods in NC, here you can call the method afterward
+
+        //Input of first number and parse it into double - input is always string
+        System.out.println("Input a first number: ...");
+        ncaroperations.a = Double.parseDouble(scanner.nextLine()); //from the string to double
+        //Input of second number and parse it into double
+        System.out.println("Enter a second number: ... ");
+        ncaroperations.b = Double.parseDouble(scanner.nextLine());
+
+        //Ask user for an operator to know what calculate
+        System.out.println("Enter a number in the way you want to calculate it:");
+        System.out.println("1 + ");
+        System.out.println("2 - ");
+        System.out.println("3 * ");
+        System.out.println("4 / ");
+        //Insert the selected number from USER into a variable (1,2,3,4,5)
+        int options = Integer.parseInt(scanner.nextLine());
+
+        //Close the scanner when you're done using it
+        //scanner.close();
+
+
 
 
 
