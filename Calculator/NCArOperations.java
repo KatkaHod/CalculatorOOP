24b34429ad9e9ac3ae8313563ee9d2699d6e5e7e
KatkaHod <hodslavska.k@gmail.com>
2023-12-15 20:33:20 +0100

    Documentation added

--- Calculator/NCArOperations.java
+++ Calculator/NCArOperations.java
@@ -1,42 +1,58 @@
 package Calculator;
 
-/** The NumberCruncherEncapsulation class encapsulates the calculator's state (operands)
- * and behavior (addition, subtraction, multiplication, and division).
- * NumberCruncher Arithmetic Operations
- * Setting up the (arithmetic) Objects  and their methods
+/**
+ * The class contains methods of arithmetic operators
+ * Addition, Subtraction, Multiplication, Division
  *  */
 
 public class NCArOperations {
-    /** The class contains methods of arithmetic operators
-     * a = first input from the user
-     * b = second input from the user
+    /**
+     * Variables double a,b
+     * a first input from the user
+     * b second input from the user
      */
-
-    //Variables of the first/second input (number) from the user
     public double a;
     public double b;
 
-    //Arithmetic calculation option number
+    //The variable options stores the selected number from the user which represent the method of calculation
     public int options;
 
-    //Setting up the Object addition
-    public double addition()
+
+    //Setting up the calculation methods
+    /**
+     * The command add
+     * Addition of variable a,b
+     * @return The sum/total of a,b
+     */
+    public double add()
     {
         return a + b;
     }
-    //Setting up the Object subtraction
-    public double subtraction()
+
+    /** The command subtract
+     * Subtraction of variable a,b
+     * @return The difference of a,b
+     */
+    public double subtract()
     {
         return a - b;
     }
 
-    //Setting up the Object multiplication
-    public double multiplication()
+    /**
+     * The command multiply
+     * Multiplication of variables a,b
+     * @return The product of a,b
+     */
+    public double multiply()
     {
         return a * b;
     }
-    //Setting up the Object division
-    public double division()
+
+    /** The command divide
+     * Division of variables a,b
+     * @return The quotient of a,b
+     */
+    public double divide()
     {
         return a / b;
 
--- Calculator/NumberCruncherApp.java
+++ Calculator/NumberCruncherApp.java
@@ -1,25 +1,28 @@
 package Calculator;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
-/** The NumberCruncherApp class serves as the entry point
- *  and interacts with the user, creating an instance of the Calculator class
- *  and performing calculations based on user input.
+/** The Class NumberCruncherApp calculates the input from the user
+ *  There is instance from the class NCArOperations to communicate with calculation methods
+ *  The calculation method is selected by the user by entering numbers between 1-4.
+ *  The output of this class-app is: Print the Total/Sum, difference, product or quotient
  *  */
 
 public class NumberCruncherApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        //Instance of Class - The purpose of this is to call the calculation methods
         NCArOperations ncaroperations = new NCArOperations();
-        //after set up the methods in NC, here you can call the method afterward
 
         //Greeting of the user
         System.out.println("Welcome to the calculator!");
 
-        String con = "yes"; // a new variable "continue" to use calculator again - User has to input YES to continue
-        while (con.equals("yes")) {  //while - execute a block of code as long as a given condition is true
+        //a new variable "continue" to use calculator again - User has to input YES to continue
+        String con = "yes";
+        //while - execute a block of code as long as a given condition is true
+        while (con.equals("yes")) {
 
         //Input of first number and parse it into double - input is always string
         System.out.println("Input a first number: ");
@@ -28,7 +31,7 @@ public class NumberCruncherApp {
         System.out.println("Enter a second number: ");
         ncaroperations.b = Double.parseDouble(scanner.nextLine());
 
-        //Ask user for an operator to know what calculate
+        //Ask user for a number that represent the calculation method
         System.out.println("Enter a number in the way you want to calculate it: ");
         System.out.println("1 + ");
         System.out.println("2 - ");
@@ -42,16 +45,16 @@ public class NumberCruncherApp {
         double result = 0;
         switch (options) {
             case 1:
-                result = ncaroperations.addition();
+                result = ncaroperations.add();
                 break;
             case 2:
-                result = ncaroperations.subtraction();
+                result = ncaroperations.subtract();
                 break;
             case 3:
-                result = ncaroperations.multiplication();
+                result = ncaroperations.multiply();
                 break;
             case 4:
-                result = ncaroperations.division();
+                result = ncaroperations.divide();
                 break;
 
             default: //In case the user enter a wrong value
